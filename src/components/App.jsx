f370cb7c6cb732b781335dc2e89d77911401dcfd
DarshaniKaushalya <darshanikaushalya7788dkd@gmail.com>
2022-02-11 16:19:36 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./heading";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+export default App;
